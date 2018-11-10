c2c348d9811729e053b17fcca5c9202e5fae3ac1
wanyuaning <wanyuaning@163.com>
2018-11-10 19:50:34 +0800

    first

---
+++ src/a.js
@@ -0,0 +1,7 @@
+import B from "./b.js"
+const txt = 'A模块运行'
+
+function a() {
+  console.log(txt)
+  B()
+}
---
+++ src/b.js
@@ -0,0 +1,5 @@
+export default function B() {
+  let txt = 'B模块'
+  console.log(`被加载的${txt}`)
+  console.log(`${txt}运行`)
+}
